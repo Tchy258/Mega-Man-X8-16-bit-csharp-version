75bc23b074b0d30059884794908598218788e3ef
Tchy258 <tchy258@yahoo.cl>
2025-01-01 16:41:55 -0300

    refactor: make animation a bit more fluid, default language en, pascal case node names

--- src/Title/DisclaimerScreen.cs
+++ src/Title/DisclaimerScreen.cs
@@ -3,38 +3,37 @@ using System;
 
 public class DisclaimerScreen : Control
 {
-    private bool exiting = false;
-    private AnimationPlayer animPlayer;
+	private bool exiting = false;
+	private AnimationPlayer animPlayer;
 
-    public override void _Ready()
-    {
-        animPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
-        UserDataFile data = GetNode<UserDataFile>("/root/UserDataFile");
-        string storedLanguage = data.CurrentLanguage;
-        data.SetLanguage(storedLanguage);
-        TranslationServer.SetLocale(storedLanguage);
-        animPlayer.Play("Fade");
-    }
+	public override void _Ready()
+	{
+		animPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		UserDataFile data = GetNode<UserDataFile>("/root/UserDataFile");
+		string storedLanguage = data.CurrentLanguage;
+		data.SetLanguage(storedLanguage);
+		TranslationServer.SetLocale(storedLanguage);
+	}
 
-    public override void _Input(InputEvent @event)
-    {
-        base._Input(@event);
-        if (@event.IsActionPressed("pause")) 
-        {
-            FadeOut();
-        }
-    }
-    public void FadeOut()
-    {
-        if (!exiting && animPlayer.CurrentAnimationPosition > 1f) 
-        {
-            exiting = true;
-            animPlayer.Seek(11f,true);
-        }
-    }
+	public override void _Input(InputEvent @event)
+	{
+		base._Input(@event);
+		if (@event.IsActionPressed("pause")) 
+		{
+			FadeOut();
+		}
+	}
+	public void FadeOut()
+	{
+		if (!exiting && animPlayer.CurrentAnimationPosition > 1f) 
+		{
+			exiting = true;
+			animPlayer.Seek(11f,true);
+		}
+	}
 
-    public void NextScreen()
-    {
-        GetTree().ChangeScene("res://src/Title/IntroAlysson.tscn");
-    }
+	public void NextScreen()
+	{
+		GetTree().ChangeScene("res://src/Title/IntroAlysson.tscn");
+	}
 }
--- src/Title/IntroAlysson.cs
+++ src/Title/IntroAlysson.cs
@@ -3,38 +3,46 @@ using System;
 
 public class IntroAlysson : Node2D
 {
-    private AnimationPlayer animPlayer;
-    private bool ableToExit = false;
-    private bool exiting = false;
+	private AnimationPlayer animPlayer;
+	private bool ableToExit = false;
+	private bool exiting = false;
+	private AudioStreamPlayer jingle;
 
-    public override void _Ready()
-    {
-        animPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
-    }
+	public override void _Ready()
+	{
+		animPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		jingle = GetNode<AudioStreamPlayer>("Jingle");
+	}
 
-    public void SetAbleToExit() 
-    {
-        ableToExit = true;
-    }
-    public override void _Input(InputEvent @event)
-    {
-        if (!ableToExit) return;
-        if (@event.IsActionPressed("fire") || @event.IsActionPressed("pause"))
-        {
-            FadeOut();
-        }
-    }
-    public void FadeOut()
-    {
-        if (!exiting && animPlayer.CurrentAnimationPosition < 6f)
-        {
-            exiting = true;
-            animPlayer.Play("FadeOut");
-        }
-    }
-    
-    public void NextScreen()
-    {
-        GetTree().ChangeScene("res://src/Title/IntroCapcom.tscn");
-    }
+	public void SetAbleToExit() 
+	{
+		ableToExit = true;
+	}
+	public override void _Input(InputEvent @event)
+	{
+		if (!ableToExit) return;
+		if (@event.IsActionPressed("fire") || @event.IsActionPressed("pause"))
+		{
+			FadeOut();
+		}
+	}
+	public void FadeOut()
+	{
+		if (!exiting)
+		{
+
+			exiting = true;
+			float currentTime = animPlayer.CurrentAnimationPosition;
+			float remaining = animPlayer.CurrentAnimationLength - currentTime;
+			remaining = remaining >= 0 ? remaining : animPlayer.CurrentAnimationLength;
+			jingle.CreateTween().TweenProperty(jingle, "volume_db",-50, remaining - 0.5f);
+			animPlayer.Play("FadeOut");
+			animPlayer.Advance(remaining);
+		}
+	}
+	
+	public void NextScreen()
+	{
+		GetTree().ChangeScene("res://src/Title/IntroCapcom.tscn");
+	}
 }
