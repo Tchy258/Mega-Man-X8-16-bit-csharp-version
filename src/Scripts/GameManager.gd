58bc0e80ad1991b904420f911472f09c294752be
Tchy258 <tchy258@yahoo.cl>
2025-01-07 01:16:25 -0300

    Version number

--- src/Scripts/GameManager.gd
+++ src/Scripts/GameManager.gd
@@ -1,7 +1,7 @@
 extends Node
 
 const codename := "X8FC"
-const version := "1.0.0.9 - Tchy & Rose Xorn's Speedrun Version V.1.0.0.7"
+const version := "1.0.0.9 - Tchy & Rose Xorn's Speedrun Version V.1.0.0.8"
 const current_demo := "16-bit"
 
 var player : Character
